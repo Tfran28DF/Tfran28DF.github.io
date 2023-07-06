f9624ba0a0536edd82929ccb6e78b7e90ebeafb6
Tfran28DF <132521459+Tfran28DF@users.noreply.github.com>
2023-07-06 18:54:20 -0500

    Add files via upload

---
+++ saludo.js
@@ -0,0 +1,17 @@
+var nombre = "Fran";
+var edad = "30";
+var presenteONo = true;
+var apellido = "Paiva";
+var altura = 1.6;
+console.log;
+"Nombre = " + nombre;
+console.log;
+"Edad = " + edad;
+console.log;
+"Presente " + presenteONo;
+console.log;
+"Apellido = " + apellido;
+console.log;
+"Altura = " + altura;
+
+//alert("saludoj" + nombre);
