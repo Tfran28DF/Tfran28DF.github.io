c36b9ab5ebf3ea153780c7fde563946960e9b9fa
Tfran28DF <132521459+Tfran28DF@users.noreply.github.com>
2023-08-17 19:58:15 -0500

    Update ejercios2.js

--- ejercios2.js
+++ ejercios2.js
@@ -1,13 +1,3 @@
-// Solicitar al usuario ingresar un número
-var numeroDecimal = parseFloat(prompt("Por favor, ingresa un número:"));
+let elemento = document.links;
 
-// Solicitar al usuario ingresar un número entero
-var numeroEntero = parseInt(prompt("Por favor, ingresa otro número:"));
-
-// Calcular la suma de los dos números ingresados
-var suma = numeroDecimal + numeroEntero;
-
-// Mostrar el resultado en pantalla
-alert("El resultado de la suma es " + suma);
-
-document.write("La suma es " + suma);
+console.log(elemento);
